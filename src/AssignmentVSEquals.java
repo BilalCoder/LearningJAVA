86263823c1125de54a2a76414b4e5115c15f13be
BilalCoder <bilalkhankhurai@gmail.com>
2022-08-02 22:38:12 +0530

    Equals vs Assignment operator in Java

---
+++ src/AssignmentVSEquals.java
@@ -0,0 +1,14 @@
+public class AssignmentVSEquals {
+    public static void main(String[] args) {
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
