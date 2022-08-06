9de5329bc4700fa06337892434afd4ab0cc0a6ff
BilalCoder <bilalkhankhurai@gmail.com>
2022-08-06 14:18:07 +0530

    SpeedConverter code excercise in Java

---
+++ src/SpeedConverter.java
@@ -0,0 +1,13 @@
+public class SpeedConverter {
+    //This is the oudemy code excercise
+    public static long toMilesPerHour(double kilometersPerHour){
+        return Math.round((1/1.609) * kilometersPerHour);
+    }
+    public static void printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+        System.out.println(kilometersPerHour+" km/h = "+(int)toMilesPerHour(kilometersPerHour)+" mi/h");
+    }
+}
