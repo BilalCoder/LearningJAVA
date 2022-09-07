810008357d3b01d5edb6518495510135c5c8b7d6
BilalCoder <bilalkhankhurai@gmail.com>
2022-09-07 09:54:37 +0530

    Composition code challenge in java

---
+++ src/com/bilal/compositionchallenge/Bedroom.java
@@ -0,0 +1,30 @@
+package com.bilal.compositionchallenge;
+
+public class Bedroom {
+
+    private String name;
+    private Wall wall1, wall2, wall3, wall4;
+    private Bed bed;
+    private Ceiling ceiling;
+    private Lamp lamp;
+
+    public Bedroom(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Bed bed, Lamp lamp) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.bed = bed;
+        this.ceiling = ceiling;
+        this.lamp = lamp;
+    }
+
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void makeBed() {
+        System.out.println("Bedroom -> Making bed | ");
+        bed.make();
+    }
+}
