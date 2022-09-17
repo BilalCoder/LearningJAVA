bfcc343800b51491bc424a5cb0227bee4ced82dc
BilalCoder <bilalkhankhurai@gmail.com>
2022-09-17 22:33:38 +0530

    Arraylist List code challenge in java

--- src/com/bilal/arraylistchallenge/MobilePhone.java
+++ src/com/bilal/arraylistchallenge/MobilePhone.java
@@ -4,89 +4,89 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
 
-    String name;
-
-    public MobilePhone(String name) {
-        this.name = name;
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
     }
 
-    private static ArrayList<Contacts> contacts = new ArrayList<>();
-    private static Scanner scanner = new Scanner(System.in);
+    public boolean addNewContact(Contact contact) {
+        if(findContact(contact.getName()) >=0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
 
-    public void showMenu() {
-        System.out.println("Enter the number corresponding to the feature: ");
-        System.out.println("0: Print the menu items");
-        System.out.println("1: Print the list of contacts");
-        System.out.println("2: Add a new contact");
-        System.out.println("3: Update existing contact");
-        System.out.println("4: Search for a contact");
-        System.out.println("5: remove a contact");
-        System.out.println("6: Remove all contacts");
-        System.out.println("7: Quit");
-        processTheChoice();
+        myContacts.add(contact);
+        return true;
+
     }
-    public void processTheChoice() {
-        boolean quit = false;
-        int choice = 0;
-        while (!quit) {
-            System.out.println("Enter your choice: ");
-            choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice) {
-                case 0:
-                    showMenu();
-                    break;
-                case 1:
-                    printContactList();
-                    break;
-                case 2:
-                    addContact();
-                    break;
-                case 3:
-                    updateContact();
-                    break;
-                case 4:
-                    removeContact();
-                    break;
-                case 5:
-                    searchContact();
-                    break;
-                case 6:
-                    clearContacts();
-                    break;
-                case 7:
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("You have entered an invalid value.");
-                    showMenu();
+
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int foundPosition = findContact(oldContact);
+        if(foundPosition <0) {
+            System.out.println(oldContact.getName() +", was not found.");
+            return false;
+        } else if(findContact(newContact.getName()) != -1) {
+            System.out.println("Contact with name " + newContact.getName() +
+                    " already exists.  Update was not successful.");
+            return false;
+        }
+
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
+        return true;
+    }
+
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if(foundPosition <0) {
+            System.out.println(contact.getName() +", was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted.");
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for(int i=0; i<this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if(contact.getName().equals(contactName)) {
+                return i;
             }
         }
+        return -1;
     }
 
-    public void printContactList() {
-        return;
+    public String queryContact(Contact contact) {
+        if(findContact(contact) >=0) {
+            return contact.getName();
+        }
+        return null;
     }
 
-    public void addContact() {
-        return;
+    public Contact queryContact(String name) {
+        int position = findContact(name);
+        if(position >=0) {
+            return this.myContacts.get(position);
+        }
+
+        return null;
     }
 
-    public void updateContact() {
-        return;
-    }
+    public void printContacts() {
+        System.out.println("Contact List");
+        for(int i=0; i<this.myContacts.size(); i++) {
+            System.out.println((i+1) + "." +
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
 
-    public void removeContact() {
-        return;
     }
-
-    public void searchContact() {
-        return;
-    }
-
-    public void clearContacts() {
-        return;
-    }
-
 }
