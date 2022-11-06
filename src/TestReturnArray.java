c43e20891d9a531d7db919b33c590ba48f0ce149
BilalCoder <bilalkhankhurai@gmail.com>
2022-11-06 15:41:04 +0530

    Array

--- src/TestReturnArray.java
+++ src/TestReturnArray.java
@@ -6,10 +6,17 @@ class TestReturnArray{
         return arr;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         //calling method which returns an array
-        int arr[]=get();
+        int arr[] = get();
         //printing the values of an array
-        for(int i=0;i<arr.length;i++)
+        for (int i = 0; i < arr.length; i++)
             System.out.println(arr[i]);
-    }}
+        System.out.println("________________________________________");
+        int arr1[] = {50, 60, 70, 80};
+        for (int i = 0; i <= arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+    }
+
+}
